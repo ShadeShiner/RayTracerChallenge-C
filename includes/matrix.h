c6bb7591580f7a4d0735e43756d88d66c838003f
ShadeShiner <adrianpadilla@outlook.com>
2020-12-22 23:50:19 -0800

    Modified matrix code to float array

--- includes/matrix.h
+++ includes/matrix.h
@@ -3,43 +3,37 @@
 
 #include "tuple.h"
 
-/* Struct Declaration */
+typedef float Matrix;
 
-typedef struct Matrix_
-{
-	unsigned int size;
-	float *matrix;
-} Matrix;
-
-
+#define array_mem(m) (&m[0][0])
 
 /* Functions */
 
 void matrix_init(Matrix *m, unsigned int size);
 
-void matrix_identity(Matrix *m);
+void matrix_identity(Matrix *m, const unsigned int size);
 
-float matrix_get(Matrix *m, unsigned int row, unsigned int col);
+#define matrix_get(m, row, col, size) (m[(row) * size + (col)])
 
-void matrix_set(Matrix *m, unsigned int row, unsigned int col, float value);
+#define matrix_set(m, row, col, size, value) (m[(row) * (size) + (col)] = (value))
 
-int matrix_equal(Matrix *a, Matrix *b);
+int matrix_equal(Matrix *left, Matrix *right, const unsigned int size);
 
-void matrix_mul(Matrix *m, Matrix *a, Matrix *b);
+void matrix_mul(Matrix *m, Matrix *a, Matrix *b, const unsigned int size);
 
 void matrix_mul_tuple(Tuple *out, Matrix *m, Tuple *t);
 
-void matrix_transpose(Matrix *out, Matrix *in);
+void matrix_transpose(Matrix *out, Matrix *in, const unsigned int size);
 
-float matrix_determinant(Matrix *m);
+void matrix_submatrix(Matrix *out, Matrix *in, int row, int col, const unsigned int size);
 
-void matrix_submatrix(Matrix *out, Matrix *in, int row, int col);
+float matrix_determinant(Matrix *m, const unsigned int size);
 
-float matrix_minor(Matrix *m, int row, int col);
+float matrix_minor(Matrix *m, int row, int col, const unsigned int size);
 
-float matrix_cofactor(Matrix *m, int row, int col);
+float matrix_cofactor(Matrix *m, int row, int col, const unsigned int size);
 
-int matrix_inverse(Matrix *out, Matrix *in);
+int matrix_inverse(Matrix *out, Matrix *in, const unsigned int size);
 
 /* Transformations */
 
--- src/matrix.c
+++ src/matrix.c
@@ -4,165 +4,161 @@
 #include "matrix.h"
 
 
-void matrix_init(Matrix *m, unsigned int size)
+void matrix_zero(Matrix *m, unsigned int size)
 {
-	m->size = size;
-	m->matrix = (float *)malloc(size * size * sizeof(float));
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size; ++j)
+            m[i * size + j] = 0;
 }
 
-void matrix_identity(Matrix *m)
+
+void matrix_identity(Matrix *m, const unsigned int size)
 {
-	matrix_init(m, 4);
-	for (int i = 0; i < 4; ++i)
-		for (int j = 0; j < 4; ++j)
-			if (i == j)
-				matrix_set(m, i, j, 1);
-			else
-				matrix_set(m, i, j, 0);
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            unsigned int index = i * size + j;
+            float value = i == j ? 1 : 0;
+            m[index] = value;
+        }
+    }
 }
 
-float matrix_get(Matrix *m, unsigned int row, unsigned int col)
+
+int matrix_equal(Matrix *left, Matrix *right, const unsigned int size)
 {
-	int index = row * m->size + col;
-	return m->matrix[index];
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            float lvalue = matrix_get(left, i, j, size);
+            float rvalue = matrix_get(right, i, j, size);
+
+            int result = float_equal(lvalue, rvalue);
+            if (result)
+                return 1;
+        }
+    }
+
+    return 0;
 }
 
-void matrix_set(Matrix *m, unsigned int row, unsigned int col, float value)
-{
-	int index = row * m->size + col;
-	m->matrix[index] = value;
-}
 
-int matrix_equal(Matrix *a, Matrix *b)
+void matrix_mul(Matrix *m, Matrix *a, Matrix *b, const unsigned int size)
 {
-	if (a->size != b->size)
-		return 1;
-	
-	for (int i = 0; i < a->size; ++i)
-	{
-		for (int j = 0; j < a->size; ++j)
-		{
-			float m = matrix_get(a, i, j);
-			float n = matrix_get(b, i, j);
-			int result = float_equal(m, n);
-			if (result)
-				return 1;
-		}
-	}
-	return 0;
-}
-
-void matrix_mul(Matrix *m, Matrix *a, Matrix *b)
-{
-	matrix_init(m, 4);
-	for (int i = 0; i < 4; ++i)
+    for (int i = 0; i < 4; ++i)
 	{
 		for (int j = 0; j < 4; ++j)
 		{
-			float value = matrix_get(a, i, 0) * matrix_get(b, 0, j) +
-						  matrix_get(a, i, 1) * matrix_get(b, 1, j) +
-						  matrix_get(a, i, 2) * matrix_get(b, 2, j) +
-					 	  matrix_get(a, i, 3) * matrix_get(b, 3, j);
-			matrix_set(m, i, j, value);
+			float value = matrix_get(a, i, 0, size) * matrix_get(b, 0, j, size) +
+						  matrix_get(a, i, 1, size) * matrix_get(b, 1, j, size) +
+						  matrix_get(a, i, 2, size) * matrix_get(b, 2, j, size) +
+					 	  matrix_get(a, i, 3, size) * matrix_get(b, 3, j, size);
+			matrix_set(m, i, j, size, value);
 		}
 	}
 }
+
 
 void matrix_mul_tuple(Tuple *out, Matrix *m, Tuple *t)
 {
 	float values[4];
 	for (int i = 0; i < 4; ++i)
 	{
-		values[i] = matrix_get(m, i, 0) * t->x +
-					matrix_get(m, i, 1) * t->y +
-					matrix_get(m, i, 2) * t->z +
-					matrix_get(m, i, 3) * t->w;
+		values[i] = matrix_get(m, i, 0, 4) * t->x +
+					matrix_get(m, i, 1, 4) * t->y +
+					matrix_get(m, i, 2, 4) * t->z +
+					matrix_get(m, i, 3, 4) * t->w;
 	}
 	tuple_init(out, values[0], values[1], values[2], values[3]);
 }
 
-void matrix_transpose(Matrix *out, Matrix *in)
+
+void matrix_transpose(Matrix *out, Matrix *in, const unsigned int size)
 {
-	matrix_init(out, in->size);
-	for (int i = 0; i < in->size; ++i)
-		for (int j = 0; j < in->size; ++j)
+	for (int i = 0; i < size; ++i)
+    {
+		for (int j = 0; j < size; ++j)
 		{
-			float value = matrix_get(in, i, j);
-			matrix_set(out, j, i, value);
+			float value = matrix_get(in, i, j, size);
+			matrix_set(out, j, i, size, value);
 		}
+    }
 }
 
-float matrix_determinant(Matrix *m)
+
+void matrix_submatrix(Matrix *out, Matrix *in, int row, int col, const unsigned int size)
 {
-	float det = 0;
-
-	if (m->size == 2)
-	{
-		float a = matrix_get(m, 0, 0);
-		float b = matrix_get(m, 0, 1);
-		float c = matrix_get(m, 1, 0);
-		float d = matrix_get(m, 1, 1);
-		det = a * d - b * c;
-	}
-	else
-	{
-		for (int col = 0; col < m->size; ++col)
-			det = det + matrix_get(m, 0, col) * matrix_cofactor(m, 0, col);
-	}
-
-	return det;
-}
-
-void matrix_submatrix(Matrix *out, Matrix *in, int row, int col)
-{
-	matrix_init(out, in->size - 1);
 	int out_i = 0;
 	int out_j = 0;
-	for (int i = 0; i < in->size; ++i)
+	for (int i = 0; i < size; ++i)
 	{
 		if (i == row)
 			continue;
-		for (int j = 0; j < in->size; ++j)
+		for (int j = 0; j < size; ++j)
 		{
 			if (j == col)
 				continue;
-			float value = matrix_get(in, i, j);
-			matrix_set(out, out_i, out_j++, value);
+			float value = matrix_get(in, i, j, size);
+			matrix_set(out, out_i, out_j++, size-1, value);
 		}
 		++out_i;
 		out_j = 0;
 	}
 }
 
-float matrix_minor(Matrix *m, int row, int col)
+
+float matrix_determinant(Matrix *m, const unsigned int size)
 {
-	Matrix sub_matrix;
-	matrix_submatrix(&sub_matrix, m, row, col);
-	float result = matrix_determinant(&sub_matrix);
-	free(sub_matrix.matrix);
+    float det = 0;
+
+	if (size == 2)
+	{
+		float a = matrix_get(m, 0, 0, size);
+		float b = matrix_get(m, 0, 1, size);
+		float c = matrix_get(m, 1, 0, size);
+		float d = matrix_get(m, 1, 1, size);
+		det = a * d - b * c;
+	}
+    else
+	{
+		for (int col = 0; col < size; ++col)
+			det = det + matrix_get(m, 0, col, size) * matrix_cofactor(m, 0, col, size);
+	}
+
+	return det;
+}
+
+
+float matrix_minor(Matrix *m, int row, int col, const unsigned int size)
+{
+	Matrix sub_matrix[size-1][size-1];
+	matrix_submatrix(array_mem(sub_matrix), m, row, col, size);
+	float result = matrix_determinant(array_mem(sub_matrix), size-1);
 	return result;
 }
 
-float matrix_cofactor(Matrix *m, int row, int col)
+
+float matrix_cofactor(Matrix *m, int row, int col, const unsigned int size)
 {
-	float minor = matrix_minor(m, row, col);
+	float minor = matrix_minor(m, row, col, size);
 	return (row + col) % 2 == 0 ? minor : -minor;
 }
 
-int matrix_inverse(Matrix *out, Matrix *in)
+
+int matrix_inverse(Matrix *out, Matrix *in, const unsigned int size)
 {
-	float det = matrix_determinant(in);
+	float det = matrix_determinant(in, size);
 	if (det == 0)
 		return 1;
 
-	matrix_init(out, in->size);
-
-	for (int i = 0; i < in->size; ++i)
+	for (int i = 0; i < size; ++i)
 	{
-		for (int j = 0; j < in->size; ++j)
+		for (int j = 0; j < size; ++j)
 		{
-			float c = matrix_cofactor(in, i, j);
-			matrix_set(out, j, i, c / matrix_determinant(in));
+			float c = matrix_cofactor(in, i, j, size);
+			matrix_set(out, j, i, size, c / matrix_determinant(in, size));
 		}
 	}
 
@@ -173,56 +169,56 @@ int matrix_inverse(Matrix *out, Matrix *in)
 
 void matrix_translation(Matrix *out, float x, float y, float z)
 {
-	matrix_identity(out);
-	matrix_set(out, 0, 3, x);
-	matrix_set(out, 1, 3, y);
-	matrix_set(out, 2, 3, z);
+	matrix_identity(out, 4);
+	matrix_set(out, 0, 3, 4, x);
+	matrix_set(out, 1, 3, 4, y);
+	matrix_set(out, 2, 3, 4, z);
 }
 
 void matrix_scaling(Matrix *out, float x, float y, float z)
 {
-	matrix_identity(out);
-	matrix_set(out, 0, 0, x);
-	matrix_set(out, 1, 1, y);
-	matrix_set(out, 2, 2, z);
+	matrix_identity(out, 4);
+	matrix_set(out, 0, 0, 4, x);
+	matrix_set(out, 1, 1, 4, y);
+	matrix_set(out, 2, 2, 4, z);
 }
 
 void matrix_rotation_x(Matrix *out, float radians)
 {
-	matrix_identity(out);
-	matrix_set(out, 1, 1, cosf(radians));
-	matrix_set(out, 1, 2, -sinf(radians));
-	matrix_set(out, 2, 1, sinf(radians));
-	matrix_set(out, 2, 2, cosf(radians));
+	matrix_identity(out, 4);
+	matrix_set(out, 1, 1, 4, cosf(radians));
+	matrix_set(out, 1, 2, 4, -sinf(radians));
+	matrix_set(out, 2, 1, 4, sinf(radians));
+	matrix_set(out, 2, 2, 4, cosf(radians));
 }
 
 void matrix_rotation_y(Matrix *out, float radians)
 {
-	matrix_identity(out);
-	matrix_set(out, 0, 0, cosf(radians));
-	matrix_set(out, 0, 2, sinf(radians));
-	matrix_set(out, 2, 0, -sinf(radians));
-	matrix_set(out, 2, 2, cos(radians));
+	matrix_identity(out, 4);
+	matrix_set(out, 0, 0, 4, cosf(radians));
+	matrix_set(out, 0, 2, 4, sinf(radians));
+	matrix_set(out, 2, 0, 4, -sinf(radians));
+	matrix_set(out, 2, 2, 4, cos(radians));
 }
 
 
 void matrix_rotation_z(Matrix *out, float radians)
 {
-	matrix_identity(out);
-	matrix_set(out, 0, 0, cosf(radians));
-	matrix_set(out, 0, 1, -sinf(radians));
-	matrix_set(out, 1, 0, sinf(radians));
-	matrix_set(out, 1, 1, cosf(radians));
+	matrix_identity(out, 4);
+	matrix_set(out, 0, 0, 4, cosf(radians));
+	matrix_set(out, 0, 1, 4, -sinf(radians));
+	matrix_set(out, 1, 0, 4, sinf(radians));
+	matrix_set(out, 1, 1, 4, cosf(radians));
 }
 
 void matrix_shearing(Matrix *out, float xy, float xz, float yx, float yz, float zx, float zy)
 {
-	matrix_identity(out);
-	matrix_set(out, 0, 1, xy);
-	matrix_set(out, 0, 2, xz);
-	matrix_set(out, 1, 0, yx);
-	matrix_set(out, 1, 2, yz);
-	matrix_set(out, 2, 0, zx);
-	matrix_set(out, 2, 1, zy);
+	matrix_identity(out, 4);
+	matrix_set(out, 0, 1, 4, xy);
+	matrix_set(out, 0, 2, 4, xz);
+	matrix_set(out, 1, 0, 4, yx);
+	matrix_set(out, 1, 2, 4, yz);
+	matrix_set(out, 2, 0, 4, zx);
+	matrix_set(out, 2, 1, 4, zy);
 }
 
